03e9bad877cb962020165dac72a5b46265c84268
Gabriel J. Eiras <eirasgabriel@outlook.com>
2025-11-09 19:16:52 -0300

    Atualização 09/11

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,36 @@
-  const dropdownHeader = document.getElementById('dropdownHeader');
-  const dropdownMenu = document.getElementById('dropdownMenu');
+  fetch("../assets/components/header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header-placeholder").innerHTML = data;
 
-  dropdownHeader.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('hidden');
-  });
+      // ===== DROPDOWN DE USUÁRIO =====
+      const dropdownHeader = document.getElementById('dropdownHeader');
+      const dropdownMenu = document.getElementById('dropdownMenu');
 
-  document.addEventListener('click', (e) => {
-    if (!dropdownHeader.contains(e.target) && !dropdownMenu.contains(e.target)) {
-      dropdownMenu.classList.add('hidden');
-    }
-  });
+      dropdownHeader.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+      });
+
+      // ===== DROPDOWN DE SAÍDA =====
+      const logoutHeader = document.getElementById('logoutDropdownHeader');
+      const logoutMenu = document.getElementById('logoutDropdownMenu');
+      const logoutButton = logoutMenu.querySelector('a'); 
+
+      logoutHeader.addEventListener('click', () => {
+        logoutMenu.classList.toggle('hidden');
+      });
+
+      logoutButton.addEventListener('click', (e) => {
+        e.preventDefault(); 
+        window.location.href = "../pages/login.html"; 
+      });
+
+      document.addEventListener('click', (e) => {
+        if (!dropdownHeader.contains(e.target) && !dropdownMenu.contains(e.target)) {
+          dropdownMenu.classList.add('hidden');
+        }
+        if (!logoutHeader.contains(e.target) && !logoutMenu.contains(e.target)) {
+          logoutMenu.classList.add('hidden');
+        }
+      });
+    });
